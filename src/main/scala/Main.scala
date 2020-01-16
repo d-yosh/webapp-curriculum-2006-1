8ef4279b9dc81c755ef99fbda554ab6598f3dc2f
d-yosh <daicedaice87@gmail.com>
2020-01-16 13:40:34 +0900

    filter を実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,9 @@
 object Main {
-  def filter[T](list: List[T])(f: T => Boolean): List[T] = ???
+  def filter[T](list: List[T])(f: T => Boolean): List[T] = {
+    list.foldLeft(Nil:List[T]) {
+      (b, a) =>
+        if (f(a)) a :: b
+        else b
+    }.reverse
+  }
 }
